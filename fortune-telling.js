da7c16eee901db6fe676995e4470877e43f0f711
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2019-04-04 22:47:00 -0700

    Impove look of the app
    - improve the look of the home page
    - relocation of pictures
    - relocation of the texts
    - create buttons using script
    - delete the button from index.html

--- fortune-telling.js
+++ fortune-telling.js
@@ -6,8 +6,25 @@ window.onload=function(){
   let godMood = 0;
   let timesPlayed = 0;
   imageDisplay = document.getElementById("imageDisplay");
+  mainSection = document.getElementById("fortune-telling");
+  //create and format start button
+  startButton = mainSection.appendChild(document.createElement('BUTTON'));
+  startButton.appendChild(document.createTextNode("Start"));
+  startButton.id = "startButton";
+  startButton.style.left="43%";
+  startButton.style.bottom="20%";
+  startButton.style.width="200px";
   startButton = document.getElementById("startButton");
+
+  //create format restart button
+  restartButton = mainSection.appendChild(document.createElement('BUTTON'));
+  restartButton.appendChild(document.createTextNode("Restart"));
+  restartButton.style.left="43%";
+  restartButton.style.bottom="20%";
+  restartButton.style.width="200px";
+  restartButton.id = "restartButton";
   restartButton = document.getElementById("restartButton");
+
   link = document.getElementById("description");
 
   imageDisplay.style.display = "none";
@@ -22,12 +39,22 @@ window.onload=function(){
   //when clicked start button dispaly rule
   function startDraw(){
     //generate a random number that'll be use to determine how long the 籤筒is displayed
-    numberOfSeconds = random(15)*1000; // maximum display time is 15 second
+    // maximum display time is 15 second
+    numberOfSeconds = random(15)*1000;
+
+    // hide the game description when start
+    document.getElementById("gameName").style.display = "none";
+    //hide the buttons
     restartButton.style.display = "none";
     startButton.style.display = "none";
+    //display image
     imageDisplay.style.display = "";
     //display the gif of user shaking the lottery box
     imageDisplay.src = "lottery.gif";
+    imageDisplay.style.left= "40%";
+    imageDisplay.style.bottom="20%";
+    imageDisplay.style.position="absolute";
+
     //display draw after x number of second
     setTimeout(getDraw, numberOfSeconds);
     //console.log(numberOfSeconds);
@@ -45,7 +72,14 @@ window.onload=function(){
      numberOfSeconds = random(15)*1000;
      //console.log(numberOfSeconds);
      imageDisplay.style.display = 'none';
-     imageDisplay.src = godChoice + ".png"; //uses the god choice number to get the choice and display on screen
+     //uses the god choice number to get the choice and display on screen
+     imageDisplay.src = godChoice + ".png";
+
+     //change the location of the image
+     imageDisplay.style.height="50%";
+     imageDisplay.style.width = 'auto';
+     imageDisplay.style.bottom= "35%";
+     imageDisplay.style.left = "48%";
      imageDisplay.style.display = "";
      //change button to continue;
      startButton.innerText = "continue";
@@ -56,7 +90,15 @@ window.onload=function(){
    function askPermission(){
      //display user asking for permission
      imageDisplay.src = "lottery2.gif";
+
      startButton.style.display = "none";
+
+     //change the location of the image
+     imageDisplay.style.width = "auto";
+     imageDisplay.style.bottom = "30%";
+     imageDisplay.style.height = "50%";
+     imageDisplay.style.left = "38%";
+
      setTimeout(checkDecision,numberOfSeconds);
    }
 
@@ -65,14 +107,14 @@ window.onload=function(){
      // 1. laugh
      // 2. Granted
      // 3. No
-	 if(timesPlayed === 3){
-		 godMood = 2;
-		 console.log("Mood:" + godMood);
-	 }
-	else{
-     godMood = random(3)+1;
-	}
-	console.log("Mood:" + godMood);
+	    if(timesPlayed === 3){
+		      godMood = 2;
+		        //console.log("Mood:" + godMood);
+          }
+      else{
+        godMood = random(3)+1;
+      }
+	     //console.log("Mood:" + godMood);
    }
    // checks the decision of god
    function checkDecision(){
@@ -81,28 +123,49 @@ window.onload=function(){
      if(godMood === 1){
        //display NO image if the number is one and display the restart button
        imageDisplay.src = "bad.png";
+       imageDisplay.style.width = "auto";
+       imageDisplay.style.bottom = "30%";
+       imageDisplay.style.height = "50%";
+       imageDisplay.style.left = "40%";
        startButton.style.display = "none";
        restartButton.style.display = "";
-		timesPlayed = timesPlayed +1;
-		console.log("Times Played:" + timesPlayed);
+		   timesPlayed = timesPlayed +1;
+		 //console.log("Times Played:" + timesPlayed);
      }
      else if (godMood === 2){
        //display the Yes image if the number is 2 and give link to the explanation
        imageDisplay.src = "good.png";
+       imageDisplay.style.width = "auto";
+       imageDisplay.style.bottom = "30%";
+       imageDisplay.style.height = "50%";
+       imageDisplay.style.left = "40%";
+
+       //change the location of the link to the description
        link.style.display = "" ;
+       link.style.bottom = "20%";
+       link.style.left = "40%";
+       link.style.position="absolute";
+       link.style.fontSize = "30px";
+
+       //restartButton.style.display="";
        document.getElementsByTagName("a").href = godChoice + ".html";
-	   console.log("Times Played:" + timesPlayed);
+	     //console.log("Times Played:" + timesPlayed);
      }
      else{
        //display the laughing image if the number is 3 and the restart button
        imageDisplay.src = "laugh.png"
+       imageDisplay.style.width = "auto";
+       imageDisplay.style.bottom = "30%";
+       imageDisplay.style.height = "50%";
+       imageDisplay.style.left = "40$";
+
        startButton.style.display = "none";
        restartButton.style.display = "";
-	   timesPlayed = timesPlayed +1;
-	   console.log("Times Played:" + timesPlayed);
+	     timesPlayed = timesPlayed +1;
+	   //console.log("Times Played:" + timesPlayed);
        }
    }
-   console.log("Times Played:" + timesPlayed);
+   //console.log("Times Played:" + timesPlayed);
    restartButton.onclick = startDraw; //restart the process
 
 
